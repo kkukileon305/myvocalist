a762e17938513e19e0da0ecdb560f862496be446
kkukileon305 <kkukileon305@gmail.com>
2025-07-08 18:39:14 +0900

    add index

--- app/grammar/[page]/page.tsx
+++ app/grammar/[page]/page.tsx
@@ -2,11 +2,15 @@ import { datas } from "@/app/grammar/[page]/data";
 import React from "react";
 import Navigator from "@/app/grammar/[page]/Navigator";
 import LoadStyle from "@/app/grammar/[page]/LoadStyle";
+import { redirect } from "next/navigation";
 
 function getArrayChunkFromArray(n: number, chunkSize: number) {
   const startIndex = (n - 1) * chunkSize;
   const endIndex = n * chunkSize;
-  return datas.slice(startIndex, endIndex);
+  return datas.slice(startIndex, endIndex).map((item, i) => ({
+    index: startIndex + i + 1, // 원본 배열 기준 index
+    item,
+  }));
 }
 
 export default async function Page({
@@ -19,6 +23,10 @@ export default async function Page({
   const { page } = await params;
   const { size } = await searchParams;
 
+  if (!parseInt(page) || !parseInt(size)) {
+    redirect("/");
+  }
+
   const contents = getArrayChunkFromArray(Number(page), Number(size));
 
   return (
@@ -26,14 +34,19 @@ export default async function Page({
       <LoadStyle />
       <Navigator page={Number(page)} size={Number(size)} />
       <div className="max-w-5xl w-full mx-auto p-2">
-        {contents.map((content, i) => (
+        {contents.map((content) => (
           <div
+            key={content.index}
             className="w-full mb-4 bg-white border-b border-gray-400 border-dotted pb-2"
-            key={i}
-            dangerouslySetInnerHTML={{
-              __html: content,
-            }}
-          />
+          >
+            <h2 className="font-bold !text-3xl mb-4">第{content.index}課</h2>
+            <div
+              key={content.index}
+              dangerouslySetInnerHTML={{
+                __html: content.item,
+              }}
+            />
+          </div>
         ))}
       </div>
     </>
